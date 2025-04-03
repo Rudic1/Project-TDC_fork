cd18b315ba747ec8ac5309571ceef2222e0319bb
Hannah Moog <hannah.moog@generic.de>
2025-04-03 16:13:26 +0200

    added interface for list repos

---
+++ code/TDC/TDC/IRepository/IListRepository.cs
@@ -0,0 +1,11 @@
+﻿using TDC.Models;
+
+namespace TDC.IRepository;
+public interface IListRepository
+{
+    public void CreateList(ToDoList list);
+    public void UpdateList(ToDoList newList, string listId);
+    public void DeleteList(string listId);
+    public ToDoList GetListById(string listId, long userId);
+    public List<ToDoList> GetAllListsForUser(long userId);
+}
