c42a2c5fdf05721bcc38e16a4096a9042cd956dd
Hannah Moog <hannah.moog@generic.de>
2025-05-07 11:35:06 +0200

    lsit repository refactored

--- code/TDC/backend/TDC.Backend/DataRepository/ListRepository.cs
+++ code/TDC/backend/TDC.Backend/DataRepository/ListRepository.cs
@@ -1,132 +1,74 @@
-﻿using TDC.Backend.IDataRepository;
+﻿using DataRepository;
+using TDC.Backend.DataRepository.Helper;
+using TDC.Backend.IDataRepository;
 using TDC.Backend.IDataRepository.Models;
 
 namespace TDC.Backend.DataRepository
 {
-    public class ListRepository : IListRepository
+    public class ListRepository(ConnectionFactory connectionFactory) : BaseRepository(connectionFactory, "[ToDoList]"), IListRepository
     {
-        private readonly string projectPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\.."));
-        private readonly string filePath;
-
-        public ListRepository()
-        {
-            var directoryPath = Path.Combine(projectPath, "TestData");
-            if (!Directory.Exists(directoryPath)) { Directory.CreateDirectory(directoryPath); }
-
-            filePath = Path.Combine(directoryPath, "lists.csv");
-            if (!File.Exists(filePath)) { File.WriteAllText(filePath, string.Empty); }
-        }
-
         public long CreateList(ToDoListDbo list)
         {
-            list.ListId = GetNewId(); // TO-DO: remove once sql is used
-            AddListToFile(list);
-            return list.ListId; //TO:DO: with database -> return new id that sql creates
+            var sql = $"INSERT INTO dbo.{this.TableName} "
+                      + $"  (Name, IsCollaborative, IsFinished)"
+                      + $"VALUES("
+                      + $"  @name, @isCollaborative, @isFinished);";
+            var parameter = new
+            {
+                name = list.Name,
+                isCollaborative = list.IsCollaborative,
+                descriptisFinishedion = list.IsCollaborative
+            };
+
+            return this.Insert<ToDoListDbo>(sql, parameter);
         }
 
         public ToDoListDbo? GetById(long listId)
         {
-            return LoadListById(listId);
-            //TO-DO: should not return nullable later, will throw sql exception in error case
+            return this.GetById<ToDoListDbo>(listId);
         }
 
         public void UpdateListTitle(long listId, string name)
         {
-            UpdateListTitleInFile(listId, name);
+            var sql = $"UPDATE dbo.{this.TableName} "
+                      + $"SET Name = @name "
+                      + $"WHERE Id = @listId;";
+
+            var parameter = new
+            {
+                listId = listId,
+                name = name
+            };
+
+            this.Execute<ToDoListDbo>(sql, parameter);
         }
+
         public void DeleteList(long listId)
         {
-            DeleteListInFile(listId);
+            var sql = $"DELETE FROM dbo.{this.TableName} "
+                      + $"WHERE Id = @listId;";
+
+            var parameter = new
+            {
+                listId = listId
+            };
+
+            this.Execute<ToDoListDbo>(sql, parameter);
         }
 
         public void FinishList(long listId)
         {
-            SetIsFinishedInFile(listId);
-        }
+            var sql = $"UPDATE dbo.{this.TableName} "
+                      + $"SET IsFinished = @IsFinished "
+                      + $"WHERE Id = @listId;";
 
-        #region privates
-        private long GetNewId()
-        {
-            //TO-DO: remove once sql is used 
-            var existingItems = GetAllLists();
-            var max = existingItems.Select(list => list.ListId).Prepend(-1).Max();
-            return max + 1;
-        }
-
-        private void SetIsFinishedInFile(long listId)
-        {
-            var lists = GetAllLists();
-            foreach (var list in lists.Where(list => list.ListId == listId))
+            var parameter = new
             {
-                list.IsFinished = true;
-            }
-            SaveAllLists(lists);
-        }
+                listId = listId,
+                isFinished = true
+            };
 
-        private void DeleteListInFile(long listId)
-        {
-            var originalLists = GetAllLists();
-            var newLists = originalLists.Where(list => list.ListId != listId).ToList();
-            SaveAllLists(newLists);
+            this.Execute<ToDoListDbo>(sql, parameter);
         }
-
-        private void UpdateListTitleInFile(long listId, string newName)
-        {
-            var lists = GetAllLists();
-            foreach (var list in lists.Where(list => list.ListId == listId))
-            {
-                list.Name = newName;
-            }
-            SaveAllLists(lists);
-        }
-
-        private void AddListToFile(ToDoListDbo list)
-        {
-            var lists = GetAllLists();
-            lists.Add(list);
-            SaveAllLists(lists.ToList());
-        }
-
-        private void SaveAllLists(List<ToDoListDbo> lists)
-        {
-            using var writer = new StreamWriter(filePath, false);
-            foreach (var list in lists)
-            {
-                writer.WriteLine(ParseToCsvLine(list));
-            }
-            writer.Close();
-        }
-
-        private static string ParseToCsvLine(ToDoListDbo list)
-        {
-            return $"{list.ListId};{list.Name};{list.IsCollaborative};{list.IsFinished}";
-        }
-
-        private ToDoListDbo? LoadListById(long listId)
-        {
-            return GetAllLists().FirstOrDefault(list => list.ListId == listId);
-        }
-
-        private List<ToDoListDbo> GetAllLists()
-        {
-            using var reader = new StreamReader(filePath);
-            var ret = new List<ToDoListDbo>();
-            while (reader.ReadLine() is { } line)
-            {
-                ret.Add(ParseToDbo(line));
-            }
-            reader.Close();
-            return ret;
-        }
-
-        private static ToDoListDbo ParseToDbo(string csvLine)
-        {
-            var elements = csvLine.Split(';');
-            return new ToDoListDbo(long.Parse(elements[0]),
-                                   elements[1],
-                                   bool.Parse(elements[2]),
-                                   bool.Parse(elements[3]));
-        }
-        #endregion
     }
 }
