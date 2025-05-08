f7a7540c248620c0d0ef816e7a007f0c3c5f5fcc
Hannah Moog <hannah.moog@generic.de>
2025-05-08 09:13:36 +0200

    implemented unit tests for list repository

--- code/TDC/TDC-Backend.Test/RepositoryTests/ListRepositoryTests.cs
+++ code/TDC/TDC-Backend.Test/RepositoryTests/ListRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using NUnit.Framework.Constraints;
 using TDC.Backend.DataRepository;
 using TDC.Backend.DataRepository.Test;
 using TDC.Backend.IDataRepository.Models;
@@ -29,7 +30,7 @@ namespace TDC.Backend.Test.RepositoryTests
         }
 
         [Test]
-        public void GetById_ReturnCorrectValues()
+        public void GetById_IdExists_ReturnCorrectValues()
         {
 
             var listId1 = this._target.CreateList(new ToDoListDbo(0, "list1", false, false));
@@ -39,6 +40,127 @@ namespace TDC.Backend.Test.RepositoryTests
             var actual = this._target.GetById(listId1);
 
             actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void GetById_IdDoesNotExist_ReturnsNull()
+        {
+            var listId1 = this._target.CreateList(new ToDoListDbo(0, "list1", false, false));
+
+            var actual = this._target.GetById(listId1 + 1);
+
+            actual.Should().BeNull();
+        }
+
+        [Test]
+        public void CreateList_AddsListToDatabase()
+        {
+            var expected1 = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(expected1);
+            expected1.Id = listId1;
+
+            var expected2 = new ToDoListDbo(0, "list2", true, false);
+            var listId2 = this._target.CreateList(expected2);
+            expected2.Id = listId2;
+
+            var actual = this._target.GetById(listId1);
+            actual.Should().BeEquivalentTo(expected1);
+
+            actual = this._target.GetById(listId2);
+            actual.Should().BeEquivalentTo(expected2);
+        }
+
+        [Test]
+        public void UpdateListTitle_CorrectListId_UpdatesListTitle()
+        {
+            var expected = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(expected);
+
+            expected.Id = listId1;
+
+            this._target.UpdateListTitle(listId1, "new-title");
+            expected.Name = "new-title";
+
+            var actual = _target.GetById(listId1);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void UpdateListTitle_WrongListId_DoesNotUpdateListTitle()
+        {
+            var expected = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(expected);
+
+            expected.Id = listId1;
+
+            this._target.UpdateListTitle(listId1 + 1, "new-title");
+
+            var actual = _target.GetById(listId1);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void DeleteList_ListIdExists_DeletesList()
+        {
+            var newList = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(newList);
+            newList.Id = listId1;
+
+            var actual = this._target.GetById(listId1);
+            actual.Should().NotBeNull();
+
+            this._target.DeleteList(listId1);
+            actual = this._target.GetById(listId1);
+            actual.Should().BeNull();
+        }
+
+        [Test]
+        public void DeleteList_ListIdDoesNotExist_DoesNotDeleteList()
+        {
+            var newList = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(newList);
+            newList.Id = listId1;
+
+            var actual = this._target.GetById(listId1);
+            actual.Should().NotBeNull();
+
+            this._target.DeleteList(listId1 + 1);
+            actual = this._target.GetById(listId1);
+            actual.Should().NotBeNull();
+        }
+
+        [Test]
+        public void FinishList_ListIdCorrect_SetsFinishedToTrue()
+        {
+            var newList = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(newList);
+            newList.Id = listId1;
+
+            var actual = this._target.GetById(listId1);
+            actual.IsFinished.Should().BeFalse();
+
+            this._target.FinishList(listId1);
+            actual = this._target.GetById(listId1);
+
+            actual.IsFinished.Should().BeTrue();
+        }
+
+        [Test]
+        public void FinishList_ListIdFalse_DoesNotSetFinishedToTrue()
+        {
+            var newList = new ToDoListDbo(0, "list1", false, false);
+            var listId1 = this._target.CreateList(newList);
+            newList.Id = listId1;
+
+            var actual = this._target.GetById(listId1);
+            actual.IsFinished.Should().BeFalse();
+
+            this._target.FinishList(listId1 + 1);
+            actual = this._target.GetById(listId1);
+
+            actual.IsFinished.Should().BeFalse();
         }
     }
 }
