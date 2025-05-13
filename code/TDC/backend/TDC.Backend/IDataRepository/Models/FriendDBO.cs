f782d1f7d3cb603ff41c50e6cdb9cf4cdbd233bb
Hannah Moog <hannah.moog@generic.de>
2025-05-13 11:59:29 +0200

    implemented repositories

---
+++ code/TDC/backend/TDC.Backend/IDataRepository/Models/FriendDBO.cs
@@ -0,0 +1,8 @@
+﻿namespace TDC.Backend.IDataRepository.Models
+{
+    public class FriendDbo
+    {
+        public string Username { get; set; }
+        public string Friend { get; set; }
+    }
+}
