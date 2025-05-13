c0a63a09432b489884fd6ee90c08e138fb498f08
Hannah Moog <hannah.moog@generic.de>
2025-05-13 12:22:53 +0200

    request repos tested

--- code/TDC/backend/TDC.Backend/IDataRepository/IFriendRepository.cs
+++ code/TDC/backend/TDC.Backend/IDataRepository/IFriendRepository.cs
@@ -4,6 +4,6 @@
     {
         public void AddFriend(string username, string friendName);
         public void RemoveFriend(string username, string friendName);
-        public List<string> GetFriendsforUser(string username);
+        public List<string> GetFriendsForUser(string username);
     }
 }
