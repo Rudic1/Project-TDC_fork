5aec1b4fa37b6443a9eef0b2ce10dfe710cef96e
Hannah Moog <hannah.moog@generic.de>
2025-05-13 13:04:44 +0200

    fixed test setup for account handler

--- code/TDC/TDC-Backend.Test/DomainTests/AccountHandlerTests/GetAccountByUsernameTests.cs
+++ code/TDC/TDC-Backend.Test/DomainTests/AccountHandlerTests/GetAccountByUsernameTests.cs
@@ -11,12 +11,16 @@ namespace TDC.Backend.Test.DomainTests.AccountHandlerTests
     {
         private AccountHandler _target;
         private IAccountRepository _accountRepository;
+        private IFriendRepository _friendRepository;
+        private IFriendRequestRepository _friendRequestRepository;
 
         [SetUp]
         public void SetUp()
         {
             _accountRepository = Substitute.For<IAccountRepository>();
-            _target = new AccountHandler(_accountRepository);
+            _friendRepository = Substitute.For<IFriendRepository>();
+            _friendRequestRepository = Substitute.For<IFriendRequestRepository>();
+            _target = new AccountHandler(_accountRepository, _friendRepository, _friendRequestRepository);
         }
 
         [Test]
