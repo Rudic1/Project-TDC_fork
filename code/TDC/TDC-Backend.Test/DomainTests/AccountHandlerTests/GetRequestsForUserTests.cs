7e954d83239e17a36e7e6ecd8c082568a0ac2eb2
Hannah Moog <hannah.moog@generic.de>
2025-05-13 13:34:18 +0200

    handler tests implemented

---
+++ code/TDC/TDC-Backend.Test/DomainTests/AccountHandlerTests/GetRequestsForUserTests.cs
@@ -0,0 +1,31 @@
+﻿
+using NSubstitute;
+using TDC.Backend.Domain;
+using TDC.Backend.IDataRepository;
+
+namespace TDC.Backend.Test.DomainTests.AccountHandlerTests
+{
+    public class GetRequestsForUserTests
+    {
+        private AccountHandler _target;
+        private IAccountRepository _accountRepository;
+        private IFriendRepository _friendRepository;
+        private IFriendRequestRepository _friendRequestRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _accountRepository = Substitute.For<IAccountRepository>();
+            _friendRepository = Substitute.For<IFriendRepository>();
+            _friendRequestRepository = Substitute.For<IFriendRequestRepository>();
+            _target = new AccountHandler(_accountRepository, _friendRepository, _friendRequestRepository);
+        }
+
+        [Test]
+        public void GetRequestsForUser_CallsRepository()
+        {
+            _target.GetRequestsForUser("test-user");
+            _friendRequestRepository.Received().GetRequestsForUser("test-user");
+        }
+    }
+}
