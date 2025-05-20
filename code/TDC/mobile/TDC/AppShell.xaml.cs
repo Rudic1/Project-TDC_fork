af9bc69e4144175ea33daeffc53fa3cfd029a137
Ninetilt <schneeberger.julian@outllok.de>
2025-05-20 08:55:52 +0200

    changed login button on main page to logout button

--- code/TDC/mobile/TDC/AppShell.xaml.cs
+++ code/TDC/mobile/TDC/AppShell.xaml.cs
@@ -4,17 +4,21 @@ namespace TDC
 {
     public partial class AppShell : Shell
     {
+        private readonly Services.UserService _userService;
         public AppShell()
         {
             InitializeComponent();
+
+            _userService = App.Services.GetRequiredService<Services.UserService>();
+
             Routing.RegisterRoute("ToDoListPage", typeof(ListView));
             Routing.RegisterRoute("LoginPage", typeof(LoginPage));
         }
         private async void LoginButton_Clicked(object sender, EventArgs e)
         {
-            // Navigiere zur LoginPage über ihre registrierte Route
-            // "//" sorgt für absolute Navigation vom Shell-Root aus
-            await Shell.Current.GoToAsync(nameof(LoginPage));
+            _userService.Logout();
+            //await Shell.Current.GoToAsync(nameof(LoginPage));
+            Application.Current.MainPage = new LoginShell();
         }
     }
 
