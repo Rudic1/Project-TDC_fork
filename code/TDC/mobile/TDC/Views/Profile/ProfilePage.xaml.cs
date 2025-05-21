2fc763264516f8b382cf613e5ed3d148e105115e
Ninetilt <schneeberger.julian@outllok.de>
2025-05-21 19:16:40 +0200

    show username in profile

--- code/TDC/mobile/TDC/Views/Profile/ProfilePage.xaml.cs
+++ code/TDC/mobile/TDC/Views/Profile/ProfilePage.xaml.cs
@@ -1,17 +1,34 @@
+using TDC.Services;
+
 namespace TDC.Views.Profile;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
+    private readonly Services.UserService _userService;
+
+    public ProfilePage()
 	{
 		InitializeComponent();
-	}
+
+        _userService = App.Services.GetRequiredService<Services.UserService>();
+        LoadUserData(_userService);
+    }
+
+    private void LoadUserData(UserService userService)
+    {
+        if (userService.IsLoggedIn)
+        {
+            UserNameLabel.Text = userService.CurrentUser!.Username;
+        }
+    }
+
     private void DescriptionEditor_TextChanged(object sender, TextChangedEventArgs e)
     {
         int remaining = 150 - e.NewTextValue?.Length ?? 0;
         CharacterCountLabel.Text = $"{remaining}";
     }
-        private async void OpenFriendList_Clicked(object sender, EventArgs e)
+
+    private async void OpenFriendList_Clicked(object sender, EventArgs e)
     {
         // TODO: Navigation to FriendListPage
         //await Shell.Current.GoToAsync("FriendListPage");
