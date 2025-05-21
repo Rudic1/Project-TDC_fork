86964005950562096d8d7a11f13c9daa5519ec4d
Ninetilt <schneeberger.julian@outllok.de>
2025-05-21 16:31:09 +0200

    profile page layout
    working description field, dummy button for friendlist

--- code/TDC/mobile/TDC/Views/Profile/ProfilePage.xaml.cs
+++ code/TDC/mobile/TDC/Views/Profile/ProfilePage.xaml.cs
@@ -6,4 +6,14 @@ public partial class ProfilePage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void DescriptionEditor_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        int remaining = 150 - e.NewTextValue?.Length ?? 0;
+        CharacterCountLabel.Text = $"{remaining} Zeichen übrig";
+    }
+        private async void OpenFriendList_Clicked(object sender, EventArgs e)
+    {
+        // TODO: Navigation to FriendListPage
+        //await Shell.Current.GoToAsync("FriendListPage");
+    }
 }
