9f23935a1f4395c47b4bc55b119bcff6b41aea05
Christian Rudisile <christian.rudisile@cas.de>
2025-05-24 22:29:02 +0200

    start implementation - add friendlistpage

---
+++ code/TDC/mobile/TDC/Views/FriendList/FriendListPage.xaml.cs
@@ -0,0 +1,29 @@
+using TDC.IService;
+using TDC.Models;
+
+namespace TDC.Views.FriendList;
+
+public partial class FriendListPage : ContentPage
+{
+    private readonly IFriendService _friendService;
+    private readonly Services.UserService _userService;
+
+    public FriendListPage(IFriendService friendService, Services.UserService userService)
+    {
+        InitializeComponent();
+
+        _friendService = friendService;
+        _userService = userService;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (_userService.CurrentUser != null)
+        {
+            var friends = await _friendService.GetFriendsForUser(_userService.CurrentUser.Username);
+            FriendsCollectionView.ItemsSource = friends;
+        }
+    }
+}
